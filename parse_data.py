d175b8e63a38c31c15e9a052b785e4f9931b6c21
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2020-04-27 20:19:05 +0200

    Add db insert/update logic

--- parse_data.py
+++ parse_data.py
@@ -1,9 +1,20 @@
 import sqlite3
 import json
+from enum import Enum
 
-comment_dump = "/home/jonas/Development/reddit_comment_dump/RC_2008-07"
+comment_dump = "/home/jonas/Development/reddit_comment_dump/RC_2015-12"
 db_conn = sqlite3.connect("comment_data.sqlite")
 index = 0
+
+class Comment_type(Enum):
+    PARENT = 1
+    REPLY = 2
+
+def get_comment_type(comment):
+    if comment["parent_id"] == comment["subreddit_id"]:
+        return Comment_type.PARENT
+    else:
+        return Comment_type.REPLY
 
 def create_database():
     c = db_conn.cursor()
@@ -20,29 +31,33 @@ def create_database():
     """)
     c.close()
 
-def row_with_field_name_exists(comment, field_name):
+def insert_comment(comment):
     c = db_conn.cursor()
-    c.execute("SELECT COUNT(*) FROM comments WHERE parent_id = ?", comment[field_name])
-    parent_in_db = c.fetchone()
-    return (bool)parent_in_db
-
-def insert_parent(comment):
-    if row_with_field_name_exists(comment, "comment_id")
-        #update
+    if (get_comment_type(comment) == Comment_type.PARENT):
+        c.execute("INSERT INTO comments (parent_id, comment_id, parent_body) VALUES (?,?,?)",comment["parent_id"],comment["comment_id"],comment["body"])
     else:
-        #insert
-        c = db_conn.cursor()
-        c.execute("""
-            INSERT INTO comments (parent_id, parent_body) VALUES (?,?)
-        """, comment["parent_id"], comment["body"])
+        if (is_highest_score_reply(comment)):
+            c.execute("INSERT INTO comments (parent_id, comment_id, reply_score, reply_body) VALUES (?,?,?,?)",comment["parent_id"],comment["comment_id"],comment["score"],comment["body"])
 
-def insert_reply(comment):
-    if row_with_field_name_exists(comment, "parent_id")
-        #update
+def update_comment(comment):
+    c = db_conn.cursor()
+    if (get_comment_type(comment) == Comment_type.PARENT):
+        c.execute("UPDATE comments SET parent_body = '?' WHERE parent_id = '?'",comment["body"],comment["comment_id"])
     else:
-        #insert
+        c.execute("UPDATE comments SET reply_body = '?' WHERE parent_id = '?'",(comment["body"],comment["parent_id"],))
 
-def highest_score_reply(comment):
+def comment_exists_in_db(comment):
+    if (get_comment_type(comment) == Comment_type.PARENT):
+        field_name = "comment_id"
+    else:
+        field_name = "parent_id"
+
+    c = db_conn.cursor()
+    c.execute("SELECT COUNT(*) FROM comments WHERE parent_id = ?",(comment[field_name],))
+    num_in_db = c.fetchone()
+    return bool(num_in_db)
+
+def is_highest_score_reply(comment):
     c = db_conn.cursor()
     c.execute("SELECT reply_score FROM comments WHERE parent_id = ?", comment["parent_id"],)
     score_from_db = c.fetchone()
@@ -51,17 +66,13 @@ def highest_score_reply(comment):
     else:
         return False
 
-def valid_for_insert(comment):
+def verify_and_insert(comment):
     if (comment["score"] >= 2):
-        if comment["parent_id"] == comment["subreddit_id"]:
-            # Insert it as any other row
-            #return True
-            insert_parent(comment)
-        elif highest_score_reply(comment):
-            insert_reply(comment)
+        # We potentially want to check for other things in the future
+        if (comment_exists_in_db(comment)):
+            update_comment(comment)
         else:
-            return False
-
+            insert_comment(commetn)
 
 create_database()
 
@@ -69,17 +80,20 @@ with open (comment_dump) as f:
     for row in f:
         index += 1
         comment = json.loads(row)
-
         # Which fields are important?
         # - body, which the actual body of the comment
         # - score, we don't want comments that are too low rated
         # - parent_id
         # - id
         # - subreddit_id, if it's the same as parent_id, then it's a top level comment
-
-        if valid_for_insert(comment):
-            insert_into_db(comment)
+        verify_and_insert(comment)
 
         if index >= 500:
             break
 
+# INSERT INTO comments (parent_id, comment_id, reply) VALUES (?,?,?)
+# INSERT INTO comments (parent_id, comment_id, parent) VALUES (?,?,?)
+
+#if extits in db: update, else insert
+# one function for each,
+# format sql query with .format, depening on if it's parent or reply
